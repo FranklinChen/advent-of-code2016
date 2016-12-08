93916ff776d08a7b29d83e15c2ece99adf9c8db3
Franklin Chen <franklinchen@franklinchen.com>
2016-12-08 18:19:43 -0500

    Skeletal pipeline inducing type definitions. More tests from given examples.

--- src/Day01.hs
+++ src/Day01.hs
@@ -2,8 +2,45 @@ module Day01
 
 where
 
-distance :: String -> Int
-distance = error "distance"
+import Control.Arrow ((>>>))
 
 main :: IO Int
 main = distance <$> readFile "inputs/day01.txt"
+
+-- | Pipeline.
+distance :: String -> Int
+distance = parse >>> walk >>> shortestDistance
+
+parse :: String -> [Segment]
+parse = error "parse"
+
+-- | Which way to turn, then how many blocks forward to move.
+type Segment = (Turn, Int)
+
+-- | Change in direction.
+data Turn = L | R
+  deriving (Show, Eq)
+
+-- | Walk a series of segments, ending up at a point in space.
+walk :: [Segment] -> Point
+walk = walkFrom initState
+
+walkFrom :: State -> [Segment] -> Point
+walkFrom = error "walkFrom"
+
+-- | State carried while walking.
+type State = (Direction, Point)
+
+-- | Direction facing.
+data Direction = North | East | South | West
+
+-- | Initial state.
+initState :: State
+initState = (North, (0, 0))
+
+-- | 2d point in space.
+type Point = (Int, Int)
+
+-- | Shortest distance from a point to the origin.
+shortestDistance :: Point -> Int
+shortestDistance (x, y) = abs x + abs y
--- test/Day01Spec.hs
+++ test/Day01Spec.hs
@@ -1,6 +1,6 @@
 module Day01Spec where
 
-import qualified Day01
+import Day01 (distance, parse, walk, shortestDistance, Turn(L, R))
 
 import Test.Hspec
 
@@ -10,9 +10,23 @@ main = hspec spec
 spec :: Spec
 spec =
   describe "Day01" $ do
-    it "Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away" $ do
-      Day01.distance "R2, L3" `shouldBe` 5
-    it "R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away" $ do
-      Day01.distance "R2, R2, R2" `shouldBe` 2
-    it "R5, L5, R5, R3 leaves you 12 blocks away" $ do
-      Day01.distance "R5, L5, R5, R3" `shouldBe` 12
+    describe "Given examples" $ do
+      it "Following R2, L3 leaves you 2 blocks East and 3 blocks North, or 5 blocks away" $ do
+        distance "R2, L3" `shouldBe` 5
+      it "R2, R2, R2 leaves you 2 blocks due South of your starting position, which is 2 blocks away" $ do
+        distance "R2, R2, R2" `shouldBe` 2
+      it "R5, L5, R5, R3 leaves you 12 blocks away" $ do
+        distance "R5, L5, R5, R3" `shouldBe` 12
+    describe "parse" $ do
+      it "R5, L5, R5, R3" $ do
+        parse "R5, L5, R5, R3" `shouldBe` [(R, 5), (L, 5), (R, 5), (R, 3)]
+    describe "walk" $ do
+      it "R2, L3" $ do
+        walk [(R, 2), (L, 3)] `shouldBe` (2, 3)
+      it "R2, R2, R2" $ do
+        walk [(R, 2), (R, 2), (R, 2)] `shouldBe` (0, -2)
+    describe "shortestDistance" $ do
+      it "(2, 3)" $ do
+        shortestDistance (2, 3) `shouldBe` 5
+      it "(-2, 3)" $ do
+        shortestDistance (-2, 3) `shouldBe` 5
